ecd6e7ba763570e0838992fa6b1742814530001a
John Ostermueller <ostermuellerj@gmail.com>
2019-03-06 15:51:36 -0600

    GUI update

--- sketch.js
+++ sketch.js
@@ -1,110 +1,80 @@
+var gui; 
+
 var inc = 0;
-var coords = 
-    [
-        "0 0 0",
-        "0 100 0",
-        "0 100 100",
-        "0 0 100",        
-        "100 0 100",        
-        "100 0 0",
-        "100 100 0",
-        "100 100 100"
-    ]
 
 var prev = "";
-var model1;
+var star, cube, pyramid;
 var doRotate = false,
+    doFill = false,
+    doStroke = true,
     drawPoints = false,
     drawModel = true,
     drawOBJ = true;
-
+var File = ['star.obj','cube.obj']
+var rotSpeed=0.001,
+    rotSpeedMin=rotSpeed,
+    rotSpeedMax=0.05,
+    rotSpeedStep=0.001;
+var fillColor = '#ffffff',
+    strokeColor = '#ff0000';
 
 //runs once before setup
 function preload() {
-    model1 = loadModel('objs/star.obj');
+    star = loadModel("objs/star.obj");
+    cube = loadModel("objs/cube.obj");    
+//    pyramid = loadModel("objs/pyramid.obj");
 }
 
 //runs once before draw
 function setup() {
+
 //    createCanvas(600, 400, WEBGL);
     var canvas = createCanvas(windowWidth, windowHeight, WEBGL);
     
-    canvas.parent('sketch-container');
+    gui = createGui('SETTINGS', 0, windowHeight-390);
+    gui.addGlobals('rotSpeed', 'doRotate', 'drawOBJ', 'File', 'doFill', 'fillColor', 'doStroke', 'strokeColor');
+//    sliderRange(0, 0.1, 0.0001);
 }
 
 //executes every frame
 function draw() {
-    console.log(doRotate);
+//    console.log(doRotate);
     orbitControl();
     
-    rotateY(inc/200); 
+    rotateY(-PI/2);
+    rotateZ(PI/6);
+    rotateY(inc*rotSpeed); 
     
     if (doRotate) inc++;
     
     background(240);
     fill(255,255,255,100);
     
-    if (drawPoints) {
-        push();
-            translate(-50,-50,0);
-            drawShape();
-            drawSpheres();
-        pop();
-    }
-    if (drawOBJ){
-        scale(200);
-        model(model1);
-    }
-}
-
-//gets input from text box, adds to coords array and prints to console
-function addPoint() {
-    var inp = document.getElementById("myText").value;
-    document.getElementById("demo").innerHTML = inp;
+    noFill();
+    if(doFill) fill(fillColor);
+    stroke(strokeColor);
     
-    if (inp != prev) {
-        coords.push(inp);
-        prev=inp;
-    }
-}
-
-//draws polys between each point in "coords"
-function drawShape() {
-    beginShape();
-    for (var i=0; i<coords.length; i++) {
-        vertex(coords[i].split(" ")[0], coords[i].split(" ")[1], coords[i].split(" ")[2]);
-    }
-    endShape();
-}
-
-//draws a sphere for each coord in "coords"
-function drawSpheres (){
-    for (var i=0; i<coords.length; i++) {
-        push();
-            translate(coords[i].split(" ")[0], coords[i].split(" ")[1], coords[i].split(" ")[2]); 
-            sphere(4);
-        pop();
-    }
-}
-
-function clearPoints() {
-    coords = [];
-}
-
-function toggle(type) {
-    switch (type) {
-        case 0:
-            doRotate = !doRotate;
+    if (drawOBJ){
+        scale(100);
+        switch(File) {
+		  case 'star.obj':
+		    model(star);
             break;
-        case 1:
-            drawPoints = !drawPoints;
+            
+		  case 'cube.obj':
+		    model(cube);
             break;
-        case 2:
-            drawOBJ = !drawOBJ;
-            break;           
+            
+//		  case 'pyramid.obj':
+//		    model(pyramid);
+//            break;
+		}    
+//        model(star);
     }
 }
 
 function windowResized() {
   canvas = resizeCanvas(windowWidth, windowHeight);
 }
+
+
